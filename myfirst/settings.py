cc136a4e3f9d2855abdd48a773a24b93d0c4805c
Iryna <iryna@Lisa-MacBook-Pro.local>
2022-05-13 00:02:51 +0300

    v2

--- myfirst/settings.py
+++ myfirst/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path, os, sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-project_root = os.paths.dirname(__file__)
+project_root = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(project_root, "apps"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
